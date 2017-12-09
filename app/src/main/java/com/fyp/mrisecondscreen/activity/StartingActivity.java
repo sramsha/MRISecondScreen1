f2b81cece03f4bab0f43124c571490fee25d9d48
Syed Rafay <rafay_syed1@yahoo.com>
2017-12-09 15:52:38 +0500

    Added splash screen and sample logo on the starting activity

--- app/src/main/java/com/fyp/mrisecondscreen/activity/StartingActivity.java
+++ app/src/main/java/com/fyp/mrisecondscreen/activity/StartingActivity.java
@@ -1,33 +1,54 @@
 package com.fyp.mrisecondscreen.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.fyp.mrisecondscreen.R;
 import com.fyp.mrisecondscreen.utils.SessionManagement;
 
 public class StartingActivity extends AppCompatActivity {
 
+    private ImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
+        logo = (ImageView) findViewById(R.id.logo);
+        Animation myanimation = AnimationUtils.loadAnimation(this, R.anim.logotransition);
+        logo.startAnimation(myanimation);
 
-        SessionManagement session = new SessionManagement(getApplicationContext());
+        final SessionManagement session = new SessionManagement(getApplicationContext());
 
-        if (session.isLoggedIn())
-        {
-            Intent myIntent = new Intent(StartingActivity.this, MainActivity.class);
-            StartingActivity.this.startActivity(myIntent);
-        }
-        else
-        {
-            Intent myIntent = new Intent(StartingActivity.this, LoginActivity.class);
-            StartingActivity.this.startActivity(myIntent);
-        }
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
 
-        // close this activity
-        finish();
+                }
+                finally{
+                    if (session.isLoggedIn())
+                    {
+                        Intent myIntent = new Intent(StartingActivity.this, MainActivity.class);
+                        StartingActivity.this.startActivity(myIntent);
+                    }
+                    else
+                    {
+                        Intent myIntent = new Intent(StartingActivity.this, LoginActivity.class);
+                        StartingActivity.this.startActivity(myIntent);
+                    }
+
+                    // close this activity
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
     }
 }
