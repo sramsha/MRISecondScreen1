f8013473e9c6093cc591d75827e7b122027898b8
Syed Rafay <rafay_syed1@yahoo.com>
2017-12-27 01:47:53 +0500

    Made some small fixes (image loading). Will now work on Supported Ads activity

---
+++ app/src/main/java/com/fyp/mrisecondscreen/activity/SupportedAds.java
@@ -0,0 +1,15 @@
+package com.fyp.mrisecondscreen.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fyp.mrisecondscreen.R;
+
+public class SupportedAds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supported_ads);
+    }
+}
